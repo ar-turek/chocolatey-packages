7d052aa943878016b09d79ac052f75f69b587c13
Artur Ciesielski <artur.ciesielski@gmail.com>
2022-03-27 03:03:38 +0000

    Add a line to README.md, add build script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # chocolatey-packages
+
+Repository containing the definitions for all packages maintained by
+https://github.com/ar-turek
---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+shopt -s expand_aliases
+source ~/.bashrc
+
+command="${1}"
+project_name="${2}"
+project_version="${3}"
+
+if [[ -z "${command}" || -z "${project_name}" || -z "${project_version}" ]]; then
+    echo "Usage: ${0} [build | push] PROJECT VERSION"
+    exit 1
+fi
+
+case "${command}" in
+    build | push)
+        ;;
+
+    *)
+        echo "Unknown command: ${command}"
+        exit 2
+        ;;
+esac
+
+current_git_ref=$(git rev-parse --abbrev-ref HEAD)
+
+git stash
+git checkout "${project_name}-v${project_version}"
+
+for subproject_name in "${project_name}"*; do
+    cd "${subproject_name}"
+
+    case "${command}" in
+        build)
+            choco pack
+            ;;
+
+        push)
+            choco push "${subproject_name}.${project_version}.nupkg"
+            ;;
+    esac
+
+    cd ..
+done
+
+git checkout "${current_git_ref}"
+git stash pop
